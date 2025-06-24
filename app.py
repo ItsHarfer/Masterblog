3a837e7e17fbb0c083f01102fe558dfa12a2da52
Martin Haferanke <martin.haferanke@googlemail.com>
2025-06-24 22:14:59 +0200

    - Adding the functionality to display the posts data on the website in Jinja Style - Creating template index.html and css file

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask
+import json
 
+from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    with open('data/posts.json', 'r') as f:
+        blog_posts = json.load(f)
+
+    return render_template('index.html', posts=blog_posts)
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
